e3416b6a29ff63b219f9ab2661a6b88287d01fee
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-31 16:52:36 -0300

    chore: Update package.json start script to use app.js instead of index.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -9,6 +9,12 @@ const port = 3000 || process.env.PORT;
 mongoose.connect('mongodb://127.0.0.1:27017/business', {
     useNewUrlParser: true,
     useUnifiedTopology: true
+}, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Connected to the database');
+    }
 });
 
 app.listen(port, () => {
