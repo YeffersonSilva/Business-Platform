c19b6ed7fe935f1c17b725de935d5b63323a7f68
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-03 14:43:27 -0300

    chore: Update package.json start script to use app.js instead of index.js and add body-parser middleware

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,22 +1,37 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-const port = 3000 || process.env.PORT;
-
-mongoose.connect('mongodb://127.0.0.1:27017/business', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, (err) => {
-    if (err) {
-        console.log(err);
-    } else {
+async function connectToDatabase() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/business', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log('Connected to the database');
-    }
-});
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    });
+        // Inicia el servidor después de que la base de datos esté conectada
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (err) {
+        console.error('Failed to connect to the database', err);
+    }
+}
+
+// Conecta a la base de datos y arranca el servidor
+connectToDatabase();
+
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*'); 
+    res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods','GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Allow','GET, PUT, POST, DELETE, OPTIONS');
+    next();
+});
