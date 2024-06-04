7170817d01bb630e334c18a670dc63ae4bae7e60
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-04 16:52:09 -0300

    feat: add Footer and Sidebar components

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import {routing} from './app.routing';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
