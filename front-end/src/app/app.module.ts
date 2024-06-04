10586d0160310e177556c78f89f67ec5599862fe
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-04 16:35:49 -0300

    chore: fix version issues, rename front-end project, and remove unnecessary files

---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {routing} from './app.routing';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- panel/src/app/components/dashboard/dashboard.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css'],
-  standalone: true // Asegúrate de que esté marcado como standalone
-})
-export class DashboardComponent { }
