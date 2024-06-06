7519d96bb727100356ea7b77f24e7afd6281b599
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-06 14:44:56 -0300

    chore: Add Collaborator model to back-end

---
+++ back-end/src/models/Collaborator.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CollaboratorSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    fullname: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: false
+    },
+    rol: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    n_document: {
+        type: String,
+        required: false
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    state: {   
+        type: Boolean,
+        default: true,
+        required: true
+    },
+    country: {
+        type: String,
+        required: false
+    }
+
+});
+
+module.exports = mongoose.model('Collaborator', CollaboratorSchema);
+
