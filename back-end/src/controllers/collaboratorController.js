66e38ce0f8e571a00b150ed6ea2666a1a07a1377
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-06 15:39:49 -0300

    feat: Add password hashing and validation for collaborator registration

--- back-end/src/controllers/collaboratorController.js
+++ back-end/src/controllers/collaboratorController.js
@@ -6,12 +6,27 @@ const jwt = require('jsonwebtoken');
 const registerCollaboratorAdmin = async (req, res) => {
     const data = req.body;
     try {
-        data.fullname = `${data.name} ${data.surname}`;
-        const collaborator = await Collaborator.create(data);
-        res.status(201).json({ data: collaborator });
+        const collaborator = await Collaborator.find({ email: data.email });
+
+        bcrypt.hash('123456', null, null, async  function(err, hash)  {
+           if (err) {
+               return res.status(500).json({ message: 'Internal server error Password' });
+           } else {
+               if (collaborator.length > 0) {
+                   return res.status(400).json({ message: 'The email is already registered' });
+               }
+               else {
+                   data.fullname = `${data.name} ${data.surname}`;
+                   data.password = hash;
+                   const collaborator = await Collaborator.create(data);
+                   res.status(201).json({ data: collaborator });
+               }
+            }
+
+        });
 
     } catch (error) {
-        return res.status(500).json({ message: 'Internal server error' });
+        return res.status(500).json({ message: 'Internal server error Try' });
     }
 
     
