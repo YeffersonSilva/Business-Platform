96c5772ee0696e4d367eb30ef4bc9f040d40d11e
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-06 16:08:00 -0300

    feat: Update CollaboratorController and jwt helper
    The CollaboratorController has been updated to use the new jwt helper for token generation during login. This improves the security and authentication process for collaborators.

--- back-end/src/controllers/collaboratorController.js
+++ back-end/src/controllers/collaboratorController.js
@@ -1,25 +1,25 @@
 require('dotenv').config();
 const Collaborator = require('../models/Collaborator');
 const bcrypt = require('bcrypt-nodejs');
-const jwt = require('jsonwebtoken');
+const jwt = require('../helpers/jwt');
 
 const registerCollaboratorAdmin = async (req, res) => {
     const data = req.body;
     try {
         const collaborator = await Collaborator.find({ email: data.email });
 
+        if (collaborator.length > 0) {
+            return res.status(400).json({ message: 'The email is already registered' });
+        }
+
         bcrypt.hash(data.password, bcrypt.genSaltSync(10), null, async function(err, hash) {
             if (err) {
                 return res.status(500).json({ message: 'Internal server error during password hashing' });
             } else {
-                if (collaborator.length > 0) {
-                    return res.status(400).json({ message: 'The email is already registered' });
-                } else {
-                    data.fullname = `${data.name} ${data.surname}`;
-                    data.password = hash;
-                    const newCollaborator = await Collaborator.create(data);
-                    res.status(201).json({ data: newCollaborator });
-                }
+                data.fullname = `${data.name} ${data.surname}`;
+                data.password = hash;
+                const newCollaborator = await Collaborator.create(data);
+                res.status(201).json({ data: newCollaborator });
             }
         });
 
@@ -36,11 +36,13 @@ const loginCollaborator = async (req, res) => {
 
         if (collaborator.length > 0) {
             bcrypt.compare(data.password, collaborator[0].password, (err, check) => {
+                if (err) {
+                    return res.status(500).json({ message: 'Internal server error during password comparison' });
+                }
+
                 if (check) {
-                    const token = jwt.sign({ id: collaborator[0]._id }, process.env.JWT_SECRET, {
-                        expiresIn: '1h'
-                    });
-                    return res.status(200).json({ message: 'Login successful', token: token });
+                    const token = jwt.createToken(collaborator[0]);
+                    return res.status(200).json({ message: 'Login successful', user: collaborator[0], token: token });
                 } else {
                     return res.status(400).json({ message: 'Error password' });
                 }
---
+++ back-end/src/helpers/jwt.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+
+const jwtSecret = process.env.JWT_SECRET;
+
+exports.createToken = (user) => {
+    const payload = {
+        sub: user._id,
+        name: user.name,
+        surname: user.surname,
+        email: user.email,
+        rol: user.rol,
+        iat: moment().unix(),
+        exp: moment().add(1, 'hour').unix()
+    }
+    return jwt.sign(payload, jwtSecret);
+}
