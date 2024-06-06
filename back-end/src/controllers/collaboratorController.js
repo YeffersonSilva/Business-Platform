39c067f918b61a54eff7827c4ed7447643bcd957
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-06 15:51:31 -0300

    feat: Add login functionality for collaborator

--- back-end/src/controllers/collaboratorController.js
+++ back-end/src/controllers/collaboratorController.js
@@ -1,39 +1,59 @@
+require('dotenv').config();
 const Collaborator = require('../models/Collaborator');
 const bcrypt = require('bcrypt-nodejs');
 const jwt = require('jsonwebtoken');
-
 
 const registerCollaboratorAdmin = async (req, res) => {
     const data = req.body;
     try {
         const collaborator = await Collaborator.find({ email: data.email });
 
-        bcrypt.hash('123456', null, null, async  function(err, hash)  {
-           if (err) {
-               return res.status(500).json({ message: 'Internal server error Password' });
-           } else {
-               if (collaborator.length > 0) {
-                   return res.status(400).json({ message: 'The email is already registered' });
-               }
-               else {
-                   data.fullname = `${data.name} ${data.surname}`;
-                   data.password = hash;
-                   const collaborator = await Collaborator.create(data);
-                   res.status(201).json({ data: collaborator });
-               }
+        bcrypt.hash(data.password, bcrypt.genSaltSync(10), null, async function(err, hash) {
+            if (err) {
+                return res.status(500).json({ message: 'Internal server error during password hashing' });
+            } else {
+                if (collaborator.length > 0) {
+                    return res.status(400).json({ message: 'The email is already registered' });
+                } else {
+                    data.fullname = `${data.name} ${data.surname}`;
+                    data.password = hash;
+                    const newCollaborator = await Collaborator.create(data);
+                    res.status(201).json({ data: newCollaborator });
+                }
             }
-
         });
 
     } catch (error) {
-        return res.status(500).json({ message: 'Internal server error Try' });
+        return res.status(500).json({ message: 'Internal server error during registration' });
     }
+}
 
-    
-    //console.log(req);
+const loginCollaborator = async (req, res) => {
+    const data = req.body;
+
+    try {
+        const collaborator = await Collaborator.find({ email: data.email });
+
+        if (collaborator.length > 0) {
+            bcrypt.compare(data.password, collaborator[0].password, (err, check) => {
+                if (check) {
+                    const token = jwt.sign({ id: collaborator[0]._id }, process.env.JWT_SECRET, {
+                        expiresIn: '1h'
+                    });
+                    return res.status(200).json({ message: 'Login successful', token: token });
+                } else {
+                    return res.status(400).json({ message: 'Error password' });
+                }
+            });
+        } else {
+            return res.status(400).json({ message: 'User does not exist' });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error during login' });
+    }
 }
 
 module.exports = {
-
-    registerCollaboratorAdmin
+    registerCollaboratorAdmin,
+    loginCollaborator
 }
--- back-end/src/routes/collaborator.js
+++ back-end/src/routes/collaborator.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 var registerCollaboratorAdmin = require('../controllers/collaboratorController');
+var loginCollaborator = require('../controllers/collaboratorController');
 
 const app = express();
 
 app.post('/registerCollaboratorAdmin', registerCollaboratorAdmin.registerCollaboratorAdmin);
+app.post('/loginCollaborator', loginCollaborator.loginCollaborator);
 
 module.exports = app;
