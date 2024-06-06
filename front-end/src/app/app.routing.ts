52615a9d2fecbb0188f2814dfa5f8b66a0b97510
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-06 16:30:17 -0300

    feat: Add LoginComponent and update routing
    The LoginComponent has been added to the app module and the routing has been updated to include a new path for the login page. This allows users to log in to the application.

--- front-end/src/app/app.routing.ts
+++ front-end/src/app/app.routing.ts
@@ -1,10 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
 
 const appRoutes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-
+  { path: '', component: LoginComponent },
 ];
 
 export const appRoutingProviders: any[] = [];
