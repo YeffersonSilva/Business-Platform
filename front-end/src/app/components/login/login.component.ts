bffe304fa75450d2cef902c4690ba46ee13ce12c
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-07 15:46:24 -0300

    feat: Update login component with improved error handling and Spanish translations

--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -9,50 +9,68 @@ declare var $: any;
 export class LoginComponent implements OnInit {
   public user: any = {};
 
-  constructor(private _collaboratorService: CollaboratorService) {}
+  constructor(private _collaboratorService: CollaboratorService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   login() {
-    if (!this.user.email) {
-      $.notify('You must enter the email', {
-        type: 'danger',
-        spacing: 10,
-        timer: 2000,
-        placement: {
-          from: 'top',
-          align: 'right',
-        },
-        delay: 1000,
-        animate: {
-          enter: 'animated ' + 'bounce',
-          exit: 'animated ' + 'bounce',
-        },
-      });
-    } else if (!this.user.password) {
-      $.notify('You must enter the password', {
-        type: 'danger',
-        spacing: 10,
-        timer: 2000,
-        placement: {
-          from: 'top',
-          align: 'right',
-        },
-        delay: 1000,
-        animate: {
-          enter: 'animated ' + 'bounce',
-          exit: 'animated ' + 'bounce',
-        },
-      });
-    } else {
-      this._collaboratorService.loginCollaboratorAdmin(this.user).subscribe(
-        (response) => {
-          console.log(response);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+      if (!this.user.email) {
+        $.notify('You must enter the email', {
+          type: 'danger',
+          spacing: 10,
+          timer: 2000,
+          placement: {
+            from: 'top',
+            align: 'right',
+          },
+          delay: 1000,
+          animate: {
+            enter: 'animated ' + 'bounce',
+            exit: 'animated ' + 'bounce',
+          },
+        });
+      } else if (!this.user.password) {
+        $.notify('You must enter the password', {
+          type: 'danger',
+          spacing: 10,
+          timer: 2000,
+          placement: {
+            from: 'top',
+            align: 'right',
+          },
+          delay: 1000,
+          animate: {
+            enter: 'animated ' + 'bounce',
+            exit: 'animated ' + 'bounce',
+          },
+        });
+      } else {
+        this._collaboratorService.loginCollaboratorAdmin(this.user).subscribe(
+          response => {
+            console.log(response);
+            if (response.data == undefined) {
+              $.notify(response.message, {
+                type: 'danger',
+                spacing: 10,
+                timer: 2000,
+                placement: {
+                  from: 'top',
+                  align: 'right',
+                },
+                delay: 1000,
+                animate: {
+                  enter: 'animated ' + 'bounce',
+                  exit: 'animated ' + 'bounce',
+                }
+              });
+            }
+            else {
+              localStorage.setItem('token', response.token);
+              localStorage.setItem('user', JSON.stringify(response.data));
+
+            }
+          }
+        );
+      }
     }
-  }
 }
