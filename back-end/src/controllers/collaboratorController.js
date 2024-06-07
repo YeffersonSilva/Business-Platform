457aa8a221c7742d39d2e5a8d73c47cb8e96e875
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-07 15:46:19 -0300

    feat: Update CollaboratorController to handle registration and login
    The CollaboratorController has been updated to handle the registration and login functionality for collaborators. This allows new collaborators to register with their email and password, and existing collaborators to log in to the application.

--- back-end/src/controllers/collaboratorController.js
+++ back-end/src/controllers/collaboratorController.js
@@ -9,22 +9,22 @@ const registerCollaboratorAdmin = async (req, res) => {
         const collaborator = await Collaborator.find({ email: data.email });
 
         if (collaborator.length > 0) {
-            return res.status(400).json({ message: 'The email is already registered' });
+            return res.status(400).send({ data: undefined, message: 'The email is already registered' });
         }
 
         bcrypt.hash(data.password, bcrypt.genSaltSync(10), null, async function(err, hash) {
             if (err) {
-                return res.status(500).json({ message: 'Internal server error during password hashing' });
+                return res.status(500).send({ data: undefined, message: 'Internal server error during password hashing' });
             } else {
                 data.fullname = `${data.name} ${data.surname}`;
                 data.password = hash;
                 const newCollaborator = await Collaborator.create(data);
-                res.status(201).json({ data: newCollaborator });
+                res.status(201).send({ data: newCollaborator });
             }
         });
 
     } catch (error) {
-        return res.status(500).json({ message: 'Internal server error during registration' });
+        return res.status(500).send({ data: undefined, message: 'Internal server error during registration' });
     }
 }
 
@@ -37,21 +37,21 @@ const loginCollaborator = async (req, res) => {
         if (collaborator.length > 0) {
             bcrypt.compare(data.password, collaborator[0].password, (err, check) => {
                 if (err) {
-                    return res.status(500).json({ message: 'Internal server error during password comparison' });
+                    return res.status(500).send({ data: undefined, message: 'Internal server error during password comparison' });
                 }
 
                 if (check) {
                     const token = jwt.createToken(collaborator[0]);
-                    return res.status(200).json({ message: 'Login successful', user: collaborator[0], token: token });
+                    return res.status(200).send({ message: 'Login successful', data: collaborator[0], token: token });
                 } else {
-                    return res.status(400).json({ message: 'Error password' });
+                    return res.status(400).send({ data: undefined, message: 'Incorrect password' });
                 }
             });
         } else {
-            return res.status(400).json({ message: 'User does not exist' });
+            return res.status(400).send({ data: undefined, message: 'User does not exist' });
         }
     } catch (error) {
-        return res.status(500).json({ message: 'Internal server error during login' });
+        return res.status(500).send({ data: undefined, message: 'Internal server error during login' });
     }
 }
 
