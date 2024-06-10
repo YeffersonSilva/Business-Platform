d579ee14da5e94d66e097ca0e51526898615f7a4
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-10 15:35:27 -0300

    feat: Add registration functionality to create-collaborator component

--- front-end/src/app/components/collaborator/create-collaborator/create-collaborator.component.ts
+++ front-end/src/app/components/collaborator/create-collaborator/create-collaborator.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-create-collaborator',
   templateUrl: './create-collaborator.component.html',
   styleUrls: ['./create-collaborator.component.css']
 })
 export class CreateCollaboratorComponent implements OnInit {
+  public collaborator: any = {
+    gender: '',
+    rol: '',
+    country: ''
+
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  registerCollaborator(registerForm:any) {
+    console.log(this.collaborator);
   }
 
 }
