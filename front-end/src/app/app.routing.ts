bc6ef13a4c9cd18e706f0a3893c62627b963644e
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-10 14:53:29 -0300

    feat: Add routes for collaborator components
    The code changes add routes for the collaborator components, including the index, create, and edit components. This allows users to navigate to these pages and perform relevant actions related to collaborators.

--- front-end/src/app/app.routing.ts
+++ front-end/src/app/app.routing.ts
@@ -2,10 +2,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
+import { IndexCollaboratorComponent } from './components/collaborator/index-collaborator/index-collaborator.component';
+import { CreateCollaboratorComponent } from './components/collaborator/create-collaborator/create-collaborator.component';
+import { EditCollaboratorComponent } from './components/collaborator/edit-collaborator/edit-collaborator.component';
 
 const appRoutes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'collaborator', component: IndexCollaboratorComponent },
+  { path: 'collaborator/create', component: CreateCollaboratorComponent },
+  { path: 'collaborator/:id', component: EditCollaboratorComponent },
   { path: '', component: LoginComponent },
+
 ];
 
 export const appRoutingProviders: any[] = [];
