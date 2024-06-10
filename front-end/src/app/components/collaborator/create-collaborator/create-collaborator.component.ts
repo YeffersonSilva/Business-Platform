b989be3f6f6193e76ad541e8be08360cb60f10eb
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-10 14:53:47 -0300

    feat: Add create-collaborator component
    This commit adds the create-collaborator component to the project. The component includes an HTML template, TypeScript file, and CSS file. This new component allows users to create new collaborators and adds functionality to the application related to managing collaborators.

---
+++ front-end/src/app/components/collaborator/create-collaborator/create-collaborator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-collaborator',
+  templateUrl: './create-collaborator.component.html',
+  styleUrls: ['./create-collaborator.component.css']
+})
+export class CreateCollaboratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
