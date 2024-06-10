ea52ea9f21ed25834727d32e2ee69888820d09f7
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-10 14:54:04 -0300

    feat: Add index-collaborator component
    This commit adds the index-collaborator component to the project. The component includes an HTML template and TypeScript file. This new component is responsible for displaying the list of collaborators in the application.

---
+++ front-end/src/app/components/collaborator/index-collaborator/index-collaborator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-index-collaborator',
+  templateUrl: './index-collaborator.component.html',
+  styleUrls: ['./index-collaborator.component.css']
+})
+export class IndexCollaboratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
