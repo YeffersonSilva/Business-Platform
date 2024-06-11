ea013f331ce407ad59267c92eaf4c9be6dc117cd
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-11 15:19:36 -0300

    feat: Add CollaboratorService and initialize data in IndexCollaboratorComponent
    The code changes in index-collaborator.component.ts import the CollaboratorService and initialize the data in the IndexCollaboratorComponent. The CollaboratorService is used to make an API call to retrieve the list of collaborators, and the retrieved data is stored in the 'collaborators' variable. This feature enhances the application by populating the collaborator list on page load.

--- front-end/src/app/components/collaborator/index-collaborator/index-collaborator.component.ts
+++ front-end/src/app/components/collaborator/index-collaborator/index-collaborator.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CollaboratorService } from 'src/app/services/collaborator.service';
+declare var $: any;
 @Component({
   selector: 'app-index-collaborator',
   templateUrl: './index-collaborator.component.html',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexCollaboratorComponent implements OnInit {
 
-  constructor() { }
+  public token= localStorage.getItem('token');
+
+  constructor(
+    private _collaboratorService: CollaboratorService,
+    
+
+  ) { }
 
   ngOnInit(): void {
   }
 
+
+  init_data() {
+    this._collaboratorService.getCollaborators(this.token).subscribe(
+      response => {
+        if (response == undefined || !response.data) {
+          this.showNotification('Error al cargar los colaboradores', 'danger');
+        } else {
+          this.collaborators = response.data;
+        }
+      },
+      error => {
+        this.showNotification('Error al cargar los colaboradores: ' + error.message, 'danger');
+      }
+    );
+  }
+  private showNotification(message: string, type: string) {
+    $.notify(message, {
+      type: type,
+      spacing: 10,
+      timer: 2000,
+      placement: {
+        from: 'top',
+        align: 'right'
+      },
+      delay: 1000,
+      animate: {
+        enter: 'animated bounce',
+        exit: 'animated bounce'
+      }
+    });
+  }
 }
