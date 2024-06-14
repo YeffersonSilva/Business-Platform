229b43ee914fa1edea4ebacd82ce0211a606bb09
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-14 15:57:18 -0300

    fix: resolve issues in routing

--- front-end/src/app/services/collaborator.service.ts
+++ front-end/src/app/services/collaborator.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { GLOBAL } from './GLOBAL';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CollaboratorService {
   public url = GLOBAL.url;
@@ -13,22 +13,54 @@ export class CollaboratorService {
     console.log(this.url);
   }
 
-  loginCollaboratorAdmin(data : any ): Observable<any> {
+  loginCollaboratorAdmin(data: any): Observable<any> {
     let headers = new HttpHeaders().set('Content-Type', 'application/json');
-    return this._http.post(this.url + 'loginCollaboratorAdmin', data, { headers: headers });
+    return this._http.post(this.url + 'loginCollaboratorAdmin', data, {
+      headers: headers,
+    });
   }
-  registerCollaboratorAdmin(data : any, token: any): Observable<any> {
-    let headers = new HttpHeaders({'Content-Type': 'application/json', 'Authorization': token})
-    return this._http.post(this.url + 'registerCollaboratorAdmin', data, { headers: headers });
-  }
-
-  getCollaborators(token : any): Observable<any> {
-    let headers = new HttpHeaders({'Content-Type': 'application/json', 'Authorization': token})
-    return this._http.get(this.url + 'getCollaborators',  { headers: headers });
-  }
-  setState(id: any,data: any,token : any): Observable<any> {
-    let headers = new HttpHeaders({'Content-Type': 'application/json', 'Authorization': token})
-    return this._http.put(this.url + 'setState/'+id,data , { headers: headers });
+  updateCollaborator(data: any, token: any): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: token,
+    });
+    return this._http.post(this.url + 'registerCollaboratorAdmin', data, {
+      headers: headers,
+    });
   }
 
+  getCollaborators(token: any): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: token,
+    });
+    return this._http.get(this.url + 'getCollaborators', { headers: headers });
+  }
+  setState(id: any, data: any, token: any): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: token,
+    });
+    return this._http.put(this.url + 'setState/' + id, data, {
+      headers: headers,
+    });
+  }
+  getDataloginCollaborator(id: any, token: any): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: token,
+    });
+    return this._http.get(this.url + 'getDataloginCollaborator/' + id, {
+      headers: headers,
+    });
+  }
+  updateCollaboratorAdmin(id: any, data: any, token: any): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: token,
+    });
+    return this._http.put(this.url + 'updateCollaboratorAdmin/' + id, data, {
+      headers: headers,
+    });
+  }
 }
