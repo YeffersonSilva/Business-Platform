5567b2e7e8f69f7aaa7ba3829a38623664fe00c0
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-24 16:05:16 -0300

    feat: Add verifyaccount component and related files
    The code changes add the `verifyaccount` component, along with its HTML, TypeScript, and spec files. This component is responsible for displaying the verification message to the user. It is integrated into the Angular application by importing it in the `app.module.ts` and `app.routing.ts` files. This addition allows users to view the verification message after clicking on the verification link sent to their email.

---
+++ front-end/src/app/components/verifyaccount/verifyaccount.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VerifyaccountComponent } from './verifyaccount.component';
+
+describe('VerifyaccountComponent', () => {
+  let component: VerifyaccountComponent;
+  let fixture: ComponentFixture<VerifyaccountComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VerifyaccountComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VerifyaccountComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
