0a721355778c90c6941d84e9a5f4a131ca267e76
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-24 15:36:24 -0300

    feat: Add jwtClient helper module for JWT token creation

---
+++ back-end/src/helpers/jwtClient.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+
+const jwtSecret = process.env.JWT_SECRET;
+
+exports.createToken = (user) => {
+    const payload = {
+        sub: user._id,
+        name: user.name,
+        surname: user.surname,
+        email: user.email,
+        type: user.type,
+        iat: moment().unix(),
+        exp: moment().add(1, 'day').unix()
+    }
+    return jwt.sign(payload, jwtSecret);
+}
