10ce99907acc78ef30328d138c1ab880cc946730
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-06-26 15:51:12 -0300

    feat: Update client data retrieval and update functionality
    The code changes in the `edit-client.component.ts` file enhance the functionality of the `EditClientComponent` by updating the client data retrieval and update logic. The `getDataloginClient` method retrieves client data based on the provided `id` and authentication token, while the `updateClient` method updates the client information using the `updateClientAdmin` endpoint. These changes improve the user experience by allowing authenticated users to view and modify client data efficiently.

--- front-end/src/app/components/client/edit-client/edit-client.component.ts
+++ front-end/src/app/components/client/edit-client/edit-client.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ClientService } from 'src/app/services/client.service';
+declare var $: any;
 
 @Component({
   selector: 'app-edit-client',
@@ -6,10 +9,91 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-client.component.css']
 })
 export class EditClientComponent implements OnInit {
+  public client: any = {
 
-  constructor() { }
+
+    gender: '',
+    country: '',
+
+  };
+  public btnUpdate = false; // Flag for the state of the register button
+  public token: any= localStorage.getItem('token'); // Get the token from localStorage
+  public id = '';
+  public load_data = true;
+  public data = false;
+
+  constructor(
+    private _router: ActivatedRoute,
+    private clientService: ClientService, // Injecting the client service
+    private route : Router
+
+  ) { }
 
   ngOnInit(): void {
+    this._router.params.subscribe((params) => {
+      this.id = params['id'];
+      this.load_data = true;
+      this.clientService
+        .getDataloginClient(this.id, this.token)
+        .subscribe(
+          (response) => {
+            if (response.data != undefined) {
+              this.client = response.data;
+              this.data = true;
+            } else {
+              this.data = false;
+            }
+            this.load_data = false;
+          },
+          (error) => {
+            console.error(error);
+            this.load_data = false;
+          }
+        );
+    });
   }
+
+  updateClient(updateForm: any) {
+    if (updateForm.valid) {
+      this.btnUpdate = true;
+      this.clientService
+        .updateClientAdmin(this.id, this.client, this.token)
+        .subscribe(
+          (response) => {
+            if (response.data != undefined) {
+              this.showNotification('Cliente se actualizó con éxito', 'success');
+              this.route.navigate(['/client']);
+            } else {
+              this.showNotification('Error al actualizar el Cliente', 'danger');
+            }
+            this.btnUpdate = false;
+          },
+          (error) => {
+            this.showNotification('Error en la actualización: ' + error.message, 'danger');
+            this.btnUpdate = false;
+          }
+        );
+    } else {
+      this.showNotification('Complete el formulario', 'danger');
+    }
+  }
+
+  private showNotification(message: string, type: string) {
+    $.notify(message, {
+      type: type,
+      spacing: 10,
+      timer: 2000,
+      placement: {
+        from: 'top',
+        align: 'right',
+      },
+      delay: 1000,
+      animate: {
+        enter: 'animated bounce',
+        exit: 'animated bounce',
+      },
+    });
+  }
+
 
 }
