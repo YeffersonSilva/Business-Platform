09a4151c5da7df72b64b44a38137e44baaabb5b9
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-07-01 15:12:31 -0300

    feat: Add functionality to create client emails in Prospercción
    This commit adds the functionality to create client emails in the Prospercción feature. It includes the creation of the `createClientEmailsProsperccion` function in the `prosperccionController.js` file, which handles the creation of client emails. It also introduces the `ClientEmails` model in the `ClientEmail.js` file, which represents client emails in the database. This enhancement allows users to send emails to clients and track their interactions within the Prospercción feature.

--- back-end/src/controllers/prosperccionController.js
+++ back-end/src/controllers/prosperccionController.js
@@ -1,5 +1,7 @@
 const ClientCall = require('../models/ClientCall');
-
+const ClientEmails = require('../models/ClientEmail');
+const Client = require('../models/Client');
+// Call
 const createClientCallProsperccion = async (req, res) => {
     if (req.user) {
         let data = req.body;
@@ -30,7 +32,99 @@ const getClientCallsProsperccion = async (req, res) => {
     }
 };
 
+// Emails
+
+const createClientEmailsProsperccion = async (req, res) => {
+    if (req.user) {
+        let data = req.body;
+        let client = await Client.findById({ _id: data.client });
+        
+        setEmail(client.fullname, data.affair, data.email, data.content);
+
+        try {
+            let email = await ClientEmails.create(data);
+            res.status(201).json({ data: email });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ data: undefined, message: 'Internal server error during client call creation' });
+        }
+    } else {
+        res.status(401).json({ data: undefined, message: 'Unauthorized' });
+    }
+};
+ 
+
+/////////////
+
+const setEmail = async (client,affair,email, content ) => {
+    console.log(`Iniciando el envío de correo a: ${email}`);
+  
+    const readHTMLFile = (filePath, callback) => {
+      fs.readFile(filePath, { encoding: 'utf-8' }, (err, html) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, html);
+        }
+      });
+    };
+  
+    const transporter = nodemailer.createTransport(smtpTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    }));
+  
+    try {
+      
+      
+  
+  
+      const filePath = path.join(process.cwd(), 'src', 'mails', 'mail_message.html');
+      console.log(`Leyendo archivo HTML de: ${filePath}`);
+  
+      readHTMLFile(filePath, (err, html) => {
+        if (err) {
+          console.error('Error leyendo el archivo HTML:', err);
+          return;
+        }
+  
+          const rest_html = ejs.render(html, { 
+              client: client,
+              affair: affair,
+              email: email,
+              content: content
+                
+         });
+        const template = handlebars.compile(rest_html);
+        const htmlToSend = template({ op: true });
+  
+        const mailOptions = {
+          from: process.env.EMAIL_USER,
+          to: email,
+          subject: affair,
+          html: htmlToSend
+        };
+  
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.error('Error enviando el correo:', error);
+          } else {
+            console.log('Correo enviado:', info.response);
+          }
+        });
+      });
+    } catch (error) {
+      console.error('Error en setEmail:', error.message);
+    }
+  };
+  
+
 module.exports = {
     createClientCallProsperccion,
-    getClientCallsProsperccion
+    getClientCallsProsperccion,
+    createClientEmailsProsperccion
 };
