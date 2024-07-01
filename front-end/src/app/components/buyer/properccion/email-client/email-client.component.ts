a47339f7b1501178578d90cde09412572ee03818
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-07-01 15:24:56 -0300

    chore: Update email-client.component.ts

--- front-end/src/app/components/buyer/properccion/email-client/email-client.component.ts
+++ front-end/src/app/components/buyer/properccion/email-client/email-client.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ClientService } from 'src/app/services/client.service';
 
 @Component({
   selector: 'app-email-client',
@@ -8,18 +9,39 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EmailClientComponent implements OnInit {
   public id = '';
+  public token = localStorage.getItem('token');
+  public data = false;
+  public loadData = true;
 
   constructor(
     private _route: ActivatedRoute,
+    private _clientService: ClientService,
 
   ) { }
 
   ngOnInit(): void {
     this._route.params.subscribe((params) => {
       this.id = params['id'];
+      this._clientService.getClientCallsProsperccion(this.id,this.token).subscribe(
+        response=>{
+          if(response.data != undefined){
+            this.data = true;
+            this.loadData = false;
+            this.init_data();
+          }else{
+            this.data = false;
+            this.loadData = false;
+          }
+        }
+      );
 
     });
 
+  }
+
+
+  init_data(){
+    // console.log(this.data);
   }
 
 }
